b5f1fab37fb929030dbc6d3a525f363616544667
Amir Samadian <amir.samadian17@gmail.com>
2025-03-01 22:55:03 -0500

    Scroll Added. Small Tweaks

--- Frontend/script.js
+++ Frontend/script.js
@@ -7,3 +7,10 @@ function closeSidebar() {
     const sidebar = document.querySelector('.phone_menu');
     sidebar.style.display = 'none';
 }
+
+
+
+document.getElementById('rollButton').addEventListener('click', function() {
+    const scroll = document.querySelector('.scroll');
+    scroll.classList.add('rolled'); // Add 'rolled' class to start the animation
+});
