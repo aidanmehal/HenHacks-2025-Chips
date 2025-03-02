6d8de798ac166e60c148bd86f7cd295e388c854e
Amir Samadian <amir.samadian17@gmail.com>
2025-03-01 21:43:04 -0500

    Hamburger menu works now

---
+++ Frontend/script.js
@@ -0,0 +1,9 @@
+/* Burger and Sidebar functionalities for header*/
+function showSidebar() {
+    const sidebar = document.querySelector('.phone_menu');
+    sidebar.style.display = 'flex';
+}
+function closeSidebar() {
+    const sidebar = document.querySelector('.phone_menu');
+    sidebar.style.display = 'none';
+}
